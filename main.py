aa0eb4c99e7fd9bc16a2fce1dd55d108b3645ff5
Megalon <125611764+MegalonVII@users.noreply.github.com>
2023-05-14 00:24:17 -0700

    Cogs

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# all imports
 import discord
 import os
-import csv
 from discord.ext import commands
 from keep_alive import keep_alive
 import random
 import pandas as pd
 import asyncio
-import datetime
-import requests
-import json
-import urllib.parse
 from utils import *
 
 # bot instantiation
