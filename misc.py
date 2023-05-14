63cc22ed665128e3f22be21263aa00b47afe2c84
Megalon <125611764+MegalonVII@users.noreply.github.com>
2023-05-14 01:06:14 -0700

    Cogs

--- misc.py
+++ misc.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 # misc commands start here
 # ping, whomuted, avi, emote
-class Misc(commands.Cog):
+class Miscellaneous(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -11,14 +11,14 @@ class Misc(commands.Cog):
     async def ping(self, ctx):
         await ctx.message.delete()
         return await ctx.send(f'Pong! {round (self.bot.latency * 1000)}ms')
-
+    
     @commands.command(name='whomuted')
     async def whomuted(self, ctx):
         try:
             return await ctx.reply(", ".join([member.name for member in ctx.guild.members if member.is_timed_out()]), mention_author=False)
         except:
             return await ctx.reply("Wups! No one is muted currently...", mention_author=False)
-
+    
     @commands.command(name='avatar', aliases=['avi'])
     async def avatar(self, ctx, member:discord.Member=None):
         member = member or ctx.author
@@ -28,7 +28,7 @@ class Misc(commands.Cog):
         e.set_image(url=member.display_avatar.url)
         e.set_footer(text=f"Requested by: {ctx.message.author.name}")
         return await ctx.reply(embed=e, mention_author=False)
-
+    
     @commands.command(name='emote')
     async def emote(self, ctx, emote:discord.Emoji):
         embed = discord.Embed(color=discord.Color.purple())
@@ -37,4 +37,4 @@ class Misc(commands.Cog):
         return await ctx.reply(embed=embed, mention_author=False)
 
 async def setup(bot):
-    await bot.add_cog(Misc(bot))
+    await bot.add_cog(Miscellaneous(bot))
