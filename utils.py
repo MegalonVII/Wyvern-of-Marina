f249ff850f32ce3884196348f7b42ac6904fdd50
Megalon <megalonsba@gmail.com>
2024-06-11 22:39:38 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -51,7 +51,8 @@ class YTDLSource(discord.PCMVolumeTransformer):
         'quiet': True,
         'no_warnings': True,
         'default_search': 'auto',
-        'source_address': '0.0.0.0'
+        'source_address': '0.0.0.0',
+        'cookies': 'cookies.txt'
     }
     YTDL_OPTIONS = common_options.copy()
     if system() == 'Darwin':
